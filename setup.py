43de1e5a8026ca8f85fdfe7b7cffbef446d4d556
VvadKochetov007 <vladyslavdrrragonkoch@qmail.com>
2020-07-29 07:10:36 +0300

    setup changes

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 import setuptools
 
 setuptools.setup(
-    name='quick_trade-VladKochetov007',
+    name='quick_trade-VladKochetov007-',
     version='1.9.1',
     packages=setuptools.find_packages(),
-    license='',
+    license='MIT',
     long_description_content_type="text/markdown",
     description="trading system",
     url='https://github.com/VvadKochetov007/quick_trade',
