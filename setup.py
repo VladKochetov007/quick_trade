695464055a7592519fce8cf854646430a22de821
VvadKochetov007 <vladyslavdrrragonkoch@qmail.com>
2020-08-01 06:46:17 +0300

    dev upd

--- setup.py
+++ setup.py
@@ -1,15 +1,11 @@
-"""used ta by Darío López Padial (Bukosabino   https://github.com/bukosabino/ta)"""
-
-
-import setuptools
 from distutils.core import setup
 
 setup(
     name='quick_trade',
     version='1.9.1',
-    packages=setuptools.find_packages(),
+    packages=['quick_trade'],
     license='cc',
-    long_description_content_type="text/markdown",
     description="trading system",
-    url='https://github.com/VladKochetov007/quick_trade'
+    url='https://github.com/VladKochetov007/quick_trade',
+    long_description=open('README.md')
 )
