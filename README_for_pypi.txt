77411a4be35f78b94c1be2deef610f2935896717
VvadKochetov007 <vladyslavdrrragonkoch@qmail.com>
2020-08-08 04:29:41 +0300

    pypi

---
+++ README_for_pypi.txt
@@ -0,0 +1,1 @@
+Please, view page on github.
--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from distutils.core import setup
+import setuptools
 
 setup(
     name='quick_trade',
-    version='2.0',
-    packages=['quick_trade'],
+    version='2.0.1',
+    packages=setuptools.find_packages(),
     license='cc',
     description="trading system",
     url='https://github.com/VladKochetov007/quick_trade',
-    long_description=open('README.md')
+    long_description=open('README_for_pypi.txt').read()
 )
