28e6cca095ef2f15bddddaa30ecdff3d065b80f3
VvadKochetov007 <vladyslavdrrragonkoch@qmail.com>
2020-08-08 05:13:16 +0300

    pypi...

--- setup.py
+++ setup.py
@@ -8,5 +8,17 @@ setup(
     license='cc',
     description="trading system",
     url='https://github.com/VladKochetov007/quick_trade',
-    long_description=open('README_for_pypi.txt').read()
+    long_description=open('README_for_pypi.txt').read(),
+    install_requires=[
+        'iexfinance==0.4.3',
+        'numpy==1.18.5',
+        'pandas==1.0.5',
+        'plotly==4.9.0',
+        'pykalman==0.9.5',
+        'scikit-learn==0.23.1',
+        'scipy==1.4.1',
+        'ta==0.5.25',
+        'tensorflow==2.2.0',
+        'tqdm==4.48.0'
+    ]
 )
