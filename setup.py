5f7bab4fb4ecbf02233e8f343c5b979e0c09b1df
VvadKochetov007 <vladyslavdrrragonkoch@qmail.com>
2020-08-09 23:55:59 +0300

    pypi test

--- setup.py
+++ setup.py
@@ -9,6 +9,18 @@ setup(
     description="trading system",
     url='https://github.com/VladKochetov007/quick_trade',
     long_description=open('README_for_pypi.txt').read(),
+    download_url='https://github.com/VladKochetov007/quick_trade/blob/master/dist/quick_trade-2.1.2.tar.gz',
+    keywords=['trading', 'tensorflow', 'keras'],
+    author='Vlad Kochetov',
+    author_email='vladyslavdrrragonkoch@gmail.com',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+      ],
     install_requires=[
         'iexfinance',
         'numpy',
@@ -20,5 +32,5 @@ setup(
         'ta',
         'tensorflow',
         'tqdm'
-    ]
+    ],
 )
