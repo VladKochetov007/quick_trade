f1f114b41fe8562e75b4f4e5ff8647a292ee30e3
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2020-12-23 14:34:51 +0200

    pypi 3.8.5

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
-import os
 from distutils.core import setup
-
 import setuptools
+
+
+with open('./README.md') as file:
+    long_desc = file.read()
 
 setup(
     name='quick_trade',
@@ -11,28 +13,31 @@ setup(
     packages=setuptools.find_packages(),
     version='3.0.1',
     description='Trading system for stocks, forex and others',
-    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
-    long_description_content_type='text/markdown',
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
     url='https://github.com/VladKochetov007/quick_trade',
     install_requires=[
-        "iexfinance==0.4.3",
-        "plotly==4.13.0",
-        "ta==0.7.0",
-        "scikit-learn==0.23.2",
-        "tensorflow==2.3.1",
-        "pykalman==0.9.5",
-        "scipy==1.5.4",
-        "numpy==1.19.4",
-        "pandas==1.1.4",
-        "python-binance==0.7.5",
-        "requests==2.25.0",
+        'iexfinance==0.5.0',
+        'plotly==4.14.1',
+        'pykalman==0.9.5',
+        'numpy==1.19.4',
+        'pandas==1.1.5',
+        'python-binance==0.7.5',
+        'requests==2.25.1',
+        'ta==0.7.0',
+        'scipy==1.5.4',
+        'scikit-learn==0.23.2',
+        'tensorflow==2.4.0',
     ],
-    download_url='https://github.com/VladKochetov007/quick_trade/archive/3.0.tar.gz',
-    keywords=['technical analysis',
-              'python3',
-              'trading',
-              'binance',
-              'trading-bot'
+    download_url='https://github.com/VladKochetov007/quick_trade/archive/3.8.5.tar.gz',
+    keywords=[
+        'technical-analysis',
+        'python3',
+        'trading',
+        'binance',
+        'trading-bot',
+        'trading',
+        'binance-trading'
               ],
     license='cc-by-sa-4.0',
     classifiers=[
