18b070df94656a0a3f36f64fbbab02cc08984e2a
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2021-06-14 16:39:19 +0300

    ccxt

--- quick_trade/trading_sys.py
+++ quick_trade/trading_sys.py
@@ -821,7 +821,7 @@ winrate: {self.winrate}%"""
         return self.backtest_out
 
     def multi_backtest(self,
-                       tickers: Iterable[str],
+                       tickers: typing.Union[typing.Sized, typing.Iterable[str]],
                        strategy_name: str = 'strategy_macd',
                        strategy_kwargs: Dict[str, Any] = {},
                        deposit: float = 10_000.0,
