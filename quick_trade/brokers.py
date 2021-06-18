53ce6bdf2a0197e7eebce5bf3466f9881713eee2
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2021-06-18 19:37:15 +0300

    debug start balance

--- quick_trade/brokers.py
+++ quick_trade/brokers.py
@@ -1,6 +1,7 @@
 import ccxt
 import pandas as pd
 from quick_trade import utils
+from typing import Dict
 
 
 class TradingClient(object):
@@ -8,9 +9,13 @@ class TradingClient(object):
     __side__: str
     ticker: str
     cls_open_orders: int = 0
+    start_balance: Dict[str, float]
+    base: str
+    quote: str
 
     def __init__(self, client: ccxt.Exchange):
         self.client = client
+        self.start_balance = client.fetch_free_balance()
 
     def order_create(self,
                      side: str,
@@ -23,6 +28,8 @@ class TradingClient(object):
             self.client.create_market_sell_order(symbol=ticker, amount=quantity)
         self.__side__ = side
         self.ticker = ticker
+        self.base = ticker.split('/')[0]
+        self.quote = ticker.split('/')[1]
         self.ordered = True
         self._add_order_count()
 
@@ -66,12 +73,16 @@ class TradingClient(object):
 
     def exit_last_order(self):
         if self.ordered:
+            base_balance = self.get_balance_ticker(self.base)
+            bet = base_balance - self.start_balance[self.base]
             if self.__side__ == 'Sell':
-                self.new_order_buy(self.ticker, self.get_balance_ticker(self.ticker.split('/')[0]),
-                                   logging=False)  # buy for all balance
+                self.new_order_buy(self.ticker,
+                                   bet,
+                                   logging=False)
             elif self.__side__ == 'Buy':
-                self.new_order_sell(self.ticker, self.get_balance_ticker(self.ticker.split('/')[0]),
-                                    logging=False)  # sell all
+                self.new_order_sell(self.ticker,
+                                    bet,
+                                    logging=False)
             self.__side__ = 'Exit'
             self.ordered = False
             utils.logger.info('client exit')
