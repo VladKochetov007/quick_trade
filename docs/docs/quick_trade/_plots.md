348ae746db09d7ccd0436179e433724a5f8a79d3
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2021-07-07 13:53:54 +0300

    normal fig

--- docs/docs/quick_trade/_plots.md
+++ docs/docs/quick_trade/_plots.md
@@ -1,8 +1,8 @@
-# _pots
+# _plots
 File for creating quick-trade charts.
 
-## make_layout
-Method for creating layouts for plotly.
+## make_figure
+Method for creating standard figure for plotly.
 
 | param  | type | description |
 | :---: | :---: | :---: |
@@ -10,6 +10,8 @@ Method for creating layouts for plotly.
 | width | Union\[int, float] | Plotly plot width |
 | template | str | template from https://plotly.com/python/templates/ |
 | row_heights | list | The ratio of the heights of the symbol data, deposit and the deposit change. |
-|returns|`plotly.graph_objs.Layout`| standard plotly layout for quick-trade |
+|returns|`plotly.graph_objs.Figure`| standard plotly figure for quick-trade |
+
+!> The number of columns must be 1, and the number of rows must be 3.
 
 ## QuickTradeGraph
--- quick_trade/_plots.py
+++ quick_trade/_plots.py
@@ -1,13 +1,14 @@
-from plotly.graph_objs import Figure, Layout
+from plotly.graph_objs import Figure
+from plotly.subplots import make_subplots
 from typing import Union, List
 import utils
 
 
 @utils.assert_logger
-def make_layout(height: Union[int, float] = 900,
+def make_figure(height: Union[int, float] = 900,
                 width: Union[int, float] = 1300,
                 template: str = 'plotly_dark',
-                row_heights: List[Union[int, float]] = [10, 16, 7]) -> Layout:
+                row_heights: List[Union[int, float]] = [10, 16, 7]) -> Figure:
     assert isinstance(height, (int, float)), 'height must be of type <int> or <float>'
     assert isinstance(width, (int, float)), 'width must be of type <int> or <float>'
     assert isinstance(template, str), 'template must be of type <str>'
@@ -15,60 +16,30 @@ def make_layout(height: Union[int, float] = 900,
     for el in row_heights:
         assert isinstance(el, (int, float)), 'row_heights must be of type <List[int, float]>'
 
-    layout = Layout()
-    layout.grid.rows = 3
-    layout.grid.columns = 1
-    layout.update(
-        dict(
-            height=height,
-            width=width,
-            template=template,
-            xaxis_rangeslider_visible=False
-        )
-    )
-    layout.xaxis.update(
-        dict(
-            title_text=utils.TIME_TITLE,
-            row=3,
-            col=1,
-            color=utils.TEXT_COLOR
-        )
-    )  # fuck
-    layout.yaxis.update(
-        dict(
-            title_text=utils.MONEYS_TITLE,
-            row=2,
-            col=1,
-            color=utils.TEXT_COLOR
-        )
-    )
-    layout.yaxis.update(
-        dict(
-            title_text=utils.RETURNS_TITLE,
-            row=3,
-            col=1,
-            color=utils.TEXT_COLOR
-        )
-    )
-    layout.yaxis.update(
-        dict(
-            title_text=utils.DATA_TITLE,
-            row=1,
-            col=1,
-            color=utils.TEXT_COLOR
-        )
-    )
-
-    return layout
+    fig = make_subplots(3, 1, row_heights=row_heights, )
+    fig.update_layout(
+        height=height,
+        width=width,
+        template=template,
+        xaxis_rangeslider_visible=False)
+    fig.update_xaxes(
+        title_text=utils.TIME_TITLE, row=3, col=1, color=utils.TEXT_COLOR)
+    fig.update_yaxes(
+        title_text=utils.MONEYS_TITLE, row=2, col=1, color=utils.TEXT_COLOR)
+    fig.update_yaxes(
+        title_text=utils.RETURNS_TITLE, row=3, col=1, color=utils.TEXT_COLOR)
+    fig.update_yaxes(
+        title_text=utils.DATA_TITLE, row=1, col=1, color=utils.TEXT_COLOR)
+    return fig
 
 
 class QuickTradeGraph(object):
     figure: Figure
 
-    def __init__(self, layout: Layout):
-        self.figure = Figure(layout=layout)
+    def __init__(self, figure):
+        self.figure = figure
+
 
 if __name__ == "__main__":
-    g = QuickTradeGraph(layout=make_layout())
+    g = QuickTradeGraph(figure=make_figure())
     g.figure.show()
-
