97596b86fc8b1849db51fa3a64c5bface89f4cc5
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2021-09-18 03:03:58 +0300

    5

--- quick_trade/utils.py
+++ quick_trade/utils.py
@@ -169,7 +169,7 @@ mean year percentage profit: {}%
 winrate: {}%
 mean deviation: {}%"""  # .format(Trader.losses, Trader.trades, Trader.profits, Trader.year_profit, Trader.winrate, Trader.mean_deviation)
 
-__version__: str = "6.6.4"
+__version__: str = "6.6.5"
 __author__: str = 'Vlad Kochetov'
 __credits__: List[str] = [
     "Hemerson Tacon -- Stack overflow",
