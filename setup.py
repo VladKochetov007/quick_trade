94de1aab77831adfeca943f8a36f7a66603429ba
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2021-11-22 01:57:41 +0200

    for pip

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages
 with open('README.md') as file:
     long_desc = file.read()
 
-__version__ = "7.0.0indev"
+__version__ = "7.0.0indev1"
 
 setup(
     name='quick_trade',
