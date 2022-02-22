86e1417fbac22a7916c9c973fc35b765660030e1
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2022-02-22 05:43:21 +0200

    strategy decorator

--- quick_trade/__init__.py
+++ quick_trade/__init__.py
@@ -6,16 +6,21 @@ from numpy import inf
 def strategy(strat):
     @wraps(strat)
     def wrapped(self, *args, **kwargs):
-        self._registered_strategy = format_arguments(func=strat, args=args, kwargs=kwargs)
         self.returns = []
         self._converted = []
         self.deposit_history = []
         self.stop_losses = []
         self.take_profits = []
         self.open_lot_prices = []
+        registered = format_arguments(func=strat, args=args, kwargs=kwargs)
+        self._registered_strategy = registered
+
         if self._use_buffer:
             self.load()
+        load_flag = self._successful_load
+
         if not self._successful_load:
+            self._successful_load = False
             strategy_output = strat(self, *args, **kwargs)
             self.returns_update()
             if not len(self.stop_losses):
@@ -26,4 +31,6 @@ def strategy(strat):
                                tp_correction=inf)
             return strategy_output
 
+        self._successful_load = load_flag
+        self._registered_strategy = registered
     return wrapped
