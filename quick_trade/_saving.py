146a2555b7ee35cb93b2c6af259507c6d4a4207e
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2022-03-11 22:12:01 +0200

    OOP

--- quick_trade/_saving.py
+++ quick_trade/_saving.py
@@ -1,5 +1,7 @@
 from json import dump, load
 from os.path import isfile
+
+
 class JSON(object):
     path: str
 
@@ -16,6 +18,22 @@ class JSON(object):
     def write(self, data, indent: int = 2):
         with open(self.path, 'w', encoding='utf-8') as file:
             dump(data, file, indent=indent, ensure_ascii=False)
+
+class Buffer:
+    def __init__(self, buffer_data=None):
+        if buffer_data is None:
+            self._buffer = {}
+        else:
+            self._buffer = buffer_data
+
+    def write(self, key, data):
+        self._buffer[key] = data
+
+    def __contains__(self, item):
+        return item in self._buffer.keys()
+
+    def read(self, key):
+        return self._buffer[key]
 
 def read_json(path: str):
     return JSON(filepath=path).read()
--- quick_trade/quick_trade_tuner/avoid_overfitting/volatility.py
+++ quick_trade/quick_trade_tuner/avoid_overfitting/volatility.py
@@ -1,55 +1,120 @@
+import numpy as np
 import typing
 
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.cluster import AffinityPropagation
-from quick_trade.quick_trade.brokers import TradingClient
+import ccxt
+from quick_trade.quick_trade._saving import Buffer  # "...saving"
 
 
-def prepare_frame(df: pd.DataFrame):
-    df = pd.DataFrame(
-        {
-            'Open': df['Open'],
-            'High': df['High'],
-            'Low': df['Low'],
-            'Close': df['Close'],
-        }
-    )
-    return df
+class DataFrameHandler:
+    def __init__(self, client=None, timeframe: str = '1d'):
+        if client is None:
+            client = TradingClient(ccxt.binance())
+        self._client = client
+        self._timeframe = timeframe
+        self._buffer = Buffer()
 
-def get_daily(client: TradingClient, ticker: str):
-    df = client.get_data_historical(ticker=ticker, interval='1d')
-    return prepare_frame(df)
+    def __prepare_frame(self):
+        self.df = pd.DataFrame(
+            {
+                'Open': self.df['Open'],
+                'High': self.df['High'],
+                'Low': self.df['Low'],
+                'Close': self.df['Close'],
+            }
+        )
 
-def get_volatility(df, period):
+    def __download(self, ticker: str):
+        self.df = self._client.get_data_historical(ticker, self._timeframe)
+
+    def download(self, ticker: str):
+        if ticker in self._buffer:
+            self.df = self._buffer.read(ticker)
+        else:
+            self.__download(ticker=ticker)
+            self.__prepare_frame()
+            self._buffer.write(key=ticker, data=self.df)
+        return self.df
+
+
+class VolatilityHandler:
+    def __init__(self, data_handler: DataFrameHandler, span_start: int = 20, span_end: int = 30, span_step: int = 2):
+        self._data_handler = data_handler
+        self._span = range(span_start, span_end, span_step)
+
+    def historical_volatility(self, ticker: str, period: int) -> pd.Series:
+        df = self._data_handler.download(ticker)
+        roll = df.rolling(period)
+        mini = roll.min()['Low'].values
+        maxi = roll.max()['High'].values
+        mean = roll.mean()
+        mean = mean.mean(axis=1).values
+
+        return pd.Series((maxi - mini) / mean)
+
+    def average_historical_volatility(self, ticker: str, period: int = 20) -> np.float64:
+        return self.historical_volatility(ticker=ticker, period=period).mean()
+
+    def period_volatility(self, ticker: str) -> pd.Series:
+        volatility = []
+        for period in self._span:
+            volatility.append(
+                self.average_historical_volatility(ticker=ticker, period=period)
+            )
+        return pd.Series(volatility, index=self._span)
+
+    def multipair_volatility(self, tickers: typing.Iterable[str]):
+        analysis = pd.DataFrame
+        for ticker in tickers:
+            analysis[ticker] = self.period_volatility(ticker)
+        return analysis
+
+
+class VolatilityScaler:
+    def __init__(self, volatility: pd.DataFrame):
+        self._scaler: MinMaxScaler = MinMaxScaler()
+        self._volatility = volatility
+
+    def __fit(self):
+        self._scaler.fit(volatility.T.values)
+
+    def scaled_analysis(self):
+        return self._scaler.fit_transform(self._volatility.T.values).T
+
+
+
+
+
+
+"""def get_volatility(df, period):##################
     roll = df.rolling(period)
     mini = roll.min()['Low'].values
     maxi = roll.max()['High'].values
     mean = roll.mean()
     mean = mean.mean(axis=1).values
 
-    return pd.Series((maxi - mini) / mean)
-
-def get_mean_volatility(df, period):
+def get_mean_volatility(df, period):###################
     return pd.Series(get_volatility(df, period)).mean()
 
-def volatility_by_period(df, start=20, stop=30, step=2):
+def volatility_by_period(df, start=20, stop=30, step=2):###############
     volatility_correlation = []
     for period in range(start, stop, step):
         volatility_correlation.append(get_mean_volatility(df, period=period))
     return pd.Series(volatility_correlation, index=range(start, stop, step))
-
-def scale_volatility(volatility: pd.DataFrame):
+def scale_volatility(volatility: pd.DataFrame):################
     scaler = MinMaxScaler()
     scaler.fit(volatility.T.values)
     scaled = scaler.fit_transform(volatility.T.values).T
     return pd.DataFrame(scaled, columns=volatility.columns, index=volatility.index)
 
-def get_scaled_analysis(client: TradingClient, tickers: typing.List[str], start=20, stop=30, step=2):
+"""
+
+def get_scaled_analysis(data_handler: DataFrameHandler, tickers: typing.List[str], start=20, stop=30, step=2):
     volatility = pd.DataFrame()
     for ticker in tickers:
-        df = get_daily(client, ticker)
-        df = prepare_frame(df)
+        df = data_handler.download(ticker)
         volatility[ticker] = volatility_by_period(df=df, start=start, stop=stop, step=step)
     return scale_volatility(volatility)
 
@@ -93,7 +158,6 @@ if __name__ == '__main__':
             try:
                 with open('../../../../dataframes/' + ticker.replace('/', '') + f'{interval}.csv') as file:
                     df = pd.read_csv(file)
-                    # print('in dir')
             except Exception as e:
                 ticker = ticker.replace('/', '')
                 start_date = datetime.datetime.strptime('5 Aug 2000', start_type)
@@ -115,6 +179,12 @@ if __name__ == '__main__':
 
 
     client = BinanceTradingClient()
+    frame_handler = DataFrameHandler(client)
+    vol_handler = VolatilityHandler(data_handler=frame_handler)
+    volatility = vol_handler.period_volatility('BTC/USDT')
+    print(volatility, type(volatility))
+    exit()
+
     tickers =['BTC/USDT',
               'MANA/USDT',
               'ETH/USDT',
@@ -162,10 +232,11 @@ if __name__ == '__main__':
               'DOT/USDT',
               'LINK/USDT',
               ]
-    volatility = get_scaled_analysis(client, tickers, stop=30, start=20)
+    volatility = get_scaled_analysis(DataFrameHandler(client), tickers, stop=30, start=20)
     volatility = mean_analysis(volatility)
     g = ValidationAnalysisGraph(make_figure(width=1400, height=700))
     clusters = pairs_clustering(volatility)
+    print(clusters)
     colors = [
         'red', 'green', 'blue', 'yellow', '#7800FF', '#F700FF', '#FFFFFF', '#18D1B5', '#3E5EDF'
     ]
