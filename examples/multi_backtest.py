bb60941b84e33a39ee0b263d46c3bd7c3d61ac46
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2022-03-14 20:58:10 +0200

    multitest

--- examples/multi_backtest.py
+++ examples/multi_backtest.py
@@ -23,14 +23,12 @@ group_2_config = [
     {'strategy_bollinger_breakout': dict(plot=False, window=165, window_dev=1.5)},
     {'strategy_bollinger_breakout': dict(plot=False, window=190, window_dev=1)},
     {'strategy_bollinger_breakout': dict(plot=False, window=185, window_dev=1)},
-    {'strategy_bollinger_breakout': dict(plot=False, window=170, window_dev=1.5)},
 ]
 group_3_config = [
     {'strategy_bollinger_breakout': dict(plot=False, window=200, window_dev=1)},
     {'strategy_bollinger_breakout': dict(plot=False, window=170, window_dev=1.5)},
     {'strategy_bollinger_breakout': dict(plot=False, window=175, window_dev=1.5)},
     {'strategy_bollinger_breakout': dict(plot=False, window=90, window_dev=1)},
-    {'strategy_bollinger_breakout': dict(plot=False, window=165, window_dev=1.5)},
 ]
 group_4_config = [
     {'strategy_bollinger_breakout': dict(plot=False, window=195, window_dev=0.5)},
@@ -41,16 +39,12 @@ group_4_config = [
 config = {
     'ETH/USDT': group_1_cnfig,
     'LTC/USDT': group_1_cnfig,
-    'BNB/USDT': group_1_cnfig,
-    'BCH/USDT': group_1_cnfig,
-    'XMR/USDT': group_1_cnfig,
 
     'MANA/USDT': group_2_config,
     'AVAX/USDT': group_2_config,
 
     'XRP/USDT': group_3_config,
     'ADA/USDT': group_3_config,
-    'BAT/USDT': group_3_config,
 
 }
 trader.multi_backtest(test_config=config,
