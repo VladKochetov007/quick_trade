a2063dc63c132448aed72b36561b699fb6f2cd5c
VladKochetov007 <vladyslavdrrragonkoch@gmail.com>
2022-04-16 06:30:11 +0300

    examples

--- examples/volatility_tuner_validation.py
+++ examples/volatility_tuner_validation.py
@@ -3,24 +3,25 @@ from quick_trade.tuner.avoid_overfitting.volatility import Tuner, split_tickers_
 from custom_client import BinanceTradingClient
 from quick_trade.tuner import Arange, Choise, GeometricProgression
 from quick_trade.tuner import bests_to_config
+from quick_trade.plots import BasePlotlyGraph, make_figure
 
 class TrainClient(BinanceTradingClient):
     def get_data_historical(self,
                             ticker: str = None,
                             limit: int = 1000,
-                            interval: str = '1h',
+                            interval: str = '30m',
                             start_type: str = '%d %b %Y'):
         df = super().get_data_historical(ticker=ticker, interval=interval)
-        return df[:len(df)//2]
+        return df[len(df)//3*2:]
 
 class ValidationClient(BinanceTradingClient):
     def get_data_historical(self,
                             ticker: str = None,
                             limit: int = 1000,
-                            interval: str = '1m',
+                            interval: str = '30m',
                             start_type: str = '%d %b %Y'):
         df = super().get_data_historical(ticker=ticker, interval=interval)
-        return df[len(df)//2:]
+        return df[:len(df)//3*2]
 
 
 tickers = ['BTC/USDT', 'ETH/USDT', 'LTC/USDT', 'ADA/USDT', 'BNB/USDT', 'XRP/USDT', 'XMR/USDT']
@@ -33,46 +34,42 @@ params = {
                 'plot': False
             }
         ],
-    'strategy_macd':
-        [
-            {
-                'slow': Arange(10, 200, 10),
-                'fast': Arange(10, 200, 10),
-                '_RULES_': 'kwargs["slow"] > kwargs["fast"]'
-            }
-        ],
-    'strategy_parabolic_SAR':
-        [
-            {
-                'plot': False,
-                'step': GeometricProgression(0.001, 1, 1.1),
-                'max_step': GeometricProgression(0.01, 10, 1.1),
-                '_RULES_': 'kwargs["step"] <= kwargs["max_step"]'
-            }
-        ],
-    'strategy_supertrend':
-        [
-            {
-                'plot': False,
-                'multiplier': GeometricProgression(1, 15, 1.1),
-                'length': Arange(1, 100, 5),
-            }
-        ],
 }
 tuner = Tuner(TrainClient(),
-              split_tickers_volatility(tickers=tickers),
-              intervals=['1h'],
+              split_tickers_volatility(tickers=tickers,
+                                       client=TrainClient()),
+              intervals=['30m'],
               strategies_kwargs=params)
 tuner.tune(ExampleStrategies,
-           update_json_path='volatility_validation2/returns-{}.json',
+           update_json_path='volatility_validation3/returns-{}.json',
            commission=0.075)
 tuner.sort_tunes('profit/deviation ratio')
 
-config = bests_to_config(tuner.get_best(10))
+config = bests_to_config(tuner.get_best(2))
+fig = BasePlotlyGraph(make_figure(rows=2))
 
-test_trader = ExampleStrategies(interval='1h')
-test_trader.set_client(ValidationClient())
-test_trader.connect_graph()
+train_trader = ExampleStrategies(interval='30m')
+train_trader.set_client(TrainClient())
 
-test_trader.multi_backtest(test_config=config,
-                           commission=0.075)
+val_trader = ExampleStrategies(interval='30m')
+val_trader.set_client(ValidationClient())
+
+for backtester in [train_trader, val_trader]:
+    backtester.connect_graph()
+
+    backtester.multi_backtest(test_config=config,
+                              commission=0.075,
+                              show=False)
+
+fig.plot_line(line=train_trader.deposit_history,
+             name='best train',
+             color='red')
+
+fig.plot_line(line=val_trader.deposit_history,
+             name='best train validation',
+             color='green',
+             _row=2)
+fig.log_y(1, 1)
+fig.log_y(2, 1)
+fig.show()
+
---
+++ examples/walkforward_validation.py
@@ -0,0 +1,86 @@
+import copy
+import pprint
+
+from quick_trade.tuner.avoid_overfitting.walkforward import WalkForward, _static_data_historical_client
+from custom_client import BinanceTradingClient
+from quick_trade.plots import BasePlotlyGraph, make_figure
+from quick_trade.tuner.avoid_overfitting.validation_analysis import slice_frame
+from quick_trade.trading_sys import ExampleStrategies
+from quick_trade.tuner import Arange, Linspace, QuickTradeTuner
+
+
+ticker = 'BTC/USDT'
+timeframe = '1h'
+validation_split = 0.
+
+fig = BasePlotlyGraph(make_figure(rows=2))
+client = BinanceTradingClient()
+df = client.get_data_historical(ticker, interval=timeframe)
+df = df[:len(df)*2//3]
+
+chunk_len = len(df)//10
+
+splits = slice_frame(df, validation_split=validation_split)
+df_train = splits['train']
+df_val = splits['val']
+
+train_client = _static_data_historical_client(BinanceTradingClient, df_train)
+WF_train = WalkForward(client=train_client,
+                       chunk_length=chunk_len)
+
+val_client = _static_data_historical_client(BinanceTradingClient, df_val)
+WF_val = WalkForward(client=val_client,
+                     chunk_length=chunk_len)
+
+tuner_configs = [
+    {
+        'strategy_bollinger_breakout':
+             [
+                 {'window_dev': 1,
+                  'plot': False,
+                  'window': Arange(20, 100, 5)}
+             ]
+    }
+]
+for cfg in tuner_configs:
+    pprint.pprint(cfg)
+    print('\n'*3)
+
+results = []
+trains = []
+
+for i, config in enumerate(copy.deepcopy(tuner_configs)):
+    WF_train.run_analysis(ticker=ticker,
+                          timeframe=timeframe,
+                          config=config,
+                          tuner_instance=QuickTradeTuner,
+                          trader_instance=ExampleStrategies,
+                          sort_by='profit/deviation ratio',
+                          commission=0.12)
+    results.append(WF_train.profit_deviation_ratio)
+    trains.append(WF_train)
+
+best_index = results.index(max(results))
+best_config = tuner_configs[best_index]
+print(trains[best_index].info())
+
+if validation_split:
+    WF_val.run_analysis(ticker=ticker,
+                        timeframe=timeframe,
+                        config=best_config,
+                        tuner_instance=QuickTradeTuner,
+                        trader_instance=ExampleStrategies,
+                        sort_by='profit/deviation ratio',
+                        commission=0.12)
+    print(WF_val.info())
+
+fig.plot_line(line=trains[best_index].equity(),
+             name='best train',
+             color='red')
+fig.plot_line(line=WF_val.equity(),
+             name='best train validation',
+             color='green',
+             _row=2)
+fig.log_y(1, 1)
+fig.log_y(2, 1)
+fig.show()
